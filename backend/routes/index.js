0f04952af2a2198c0a29852cf90522e01edf9508
aabbddoo11 <manoo5521@gmail.com>
2026-09-20 01:38:25 +0300

    touchwood

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import router from './productRoutes.js';
-import favoriteRoutes from './FavoriteRoutes.js';
-import cartRout from './cartRoutes.js';
+
 import ordersRouter from './orderRoutes.js';
 import checkoutRouter from './checkoutRoutes.js';
 import authRouter from './authRoutes.js';
@@ -10,8 +9,7 @@ import notificationRouter from "./notificationRoutes.js";
 import reviewRouter from "./reviewRoutes.js";
 const Router = express.Router()
 Router.use("/products", router)
-Router.use("/favorites", favoriteRoutes);
-Router.use("/cart", cartRout)
+
 Router.use("/orders", ordersRouter);
 Router.use("/checkout", checkoutRouter);
 Router.use("/user", authRouter)
