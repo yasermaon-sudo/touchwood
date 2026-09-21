497615c3f5f67194ece281db785cbb8b0ecf798e
aabbddoo11 <manoo5521@gmail.com>
2026-09-21 17:56:35 +0300

    Track backend source files

--- backend/middleware/adminMiddleware.js
+++ backend/middleware/adminMiddleware.js
@@ -1,9 +1,1 @@
-export const adminMiddleware = (req, res, next) => {
-    if (req.user.role !== "admin") {
-        return res.status(403).json({
-            message: "Access denied"
-        });
-    }
-
-    next();
-};
+export const adminMiddleware = (req, res, next) => { if (!req.user || req.user.role !== "admin") { return res.status(403).json({ message: "Access denied", }); } next(); };
